fe9dd69f873e828c4119e29a2376fbcbbea2b38a
Jonathan Rangel <jonathanrangelb@gmail.com>
2021-10-13 23:12:26 -0500

    Adding more pages to the site. Adding some dynamic content.

--- index.js
+++ index.js
@@ -5,14 +5,24 @@ const app = express()
 
 const PORT = process.env.port || 3000
 
-// Agregar el router
-app.use("/",router)
+// Habilitar pug
+app.set("view engine","pug")
+
+
+// Obtener el año actual
+// Las variables siempre se deben definir antes de utilizar el routing/vistas, caso contrario las variables asignadas alos response no seran visibles mas adelante y no habra valor que mostrar en el HTML porque el proceso de hacer un res.render() va a tomar todo lo que tenga y hara el render, asi que de nada servira crear una variable despues del res.render()
+app.use((req,res,next) => {
+    const year = new Date().getFullYear()
+    res.locals.actualYear = year;
+    res.locals.nombreSitio = "Agencia de Viajes"
+    next();
+})
 
 // Definir carpeta publica
 app.use(express.static("public"))
 
-// Habilitar pug
-app.set("view engine","pug")
+// Agregar el router
+app.use("/",router)
 
 app.listen(PORT, () => {
     console.log(`El servidor se esta ejecutando en http://localhost:${PORT}`)
